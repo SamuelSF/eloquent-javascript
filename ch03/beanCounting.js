f23ac5b4d184e7e1ad012a28a808a73a9be74b02
Samuel Frade <SamuelSFrade@gmail.com>
2016-05-31 15:26:46 -0400

    Do ch03beanCounting

---
+++ ch03/beanCounting.js
@@ -0,0 +1,17 @@
+function countChar(aString, aChar){
+    charCount = 0
+    for (var i = 0; i < aString.length; i++){
+        if (aString.charAt(i) === aChar){
+            charCount += 1;
+        }
+    }
+    return charCount;
+}
+
+function countBs(aString){
+    return countChar(aString, "B");
+}
+
+
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak", "k"));
